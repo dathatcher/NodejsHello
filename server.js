5681f9a5db325df43d511b5b8da4ed974722a707
DaveT <dathatcher@chubb.com>
2018-09-01 09:08:08 -0400

    modified:   server.js

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var http = require('http');
 var port = process.env.port || 1337;
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World change in git\n');
+    res.end('Hello World change in git- from local\n');
 }).listen(port);
