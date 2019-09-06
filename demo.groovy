0b535cd83d6880fe1dffa3a2140b7c18eaa2c84e
David A. Thatcher <david.austin.thatcher@gmail.com>
2019-09-06 12:09:16 -0400

    Create demo.groovy

---
+++ demo.groovy
@@ -0,0 +1,41 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Build') {
+            steps {
+                echo 'Building..'
+            }
+        }
+        stage('Unit Test') {
+            steps {
+                echo 'Unit Testing..'
+            }
+        }
+		stage('Packaging Artifact') {
+            steps {
+                echo 'Packaging....'
+            }
+        }
+        stage('Deploy Atrifact Development') {
+            steps {
+                echo 'Deploying....'
+            }
+        }
+		stage('Smoke Test') {
+            steps {
+                echo 'Smoke Test....'
+            }
+        }
+		stage('Deploy System Test') {
+            steps {
+                echo 'Deploying....'
+            }
+        }
+		stage('Functional Testing') {
+            steps {
+                echo 'Functional testing....'
+            }
+        }
+    }
+}
