70d0e4fd81f81d381b6ed4ab2e4a4d237e79e843
David A. Thatcher <david.austin.thatcher@gmail.com>
2020-04-03 13:55:57 +0000

    Feature 110

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ var port = process.env.port || 1337;
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World - Infra as code demo \n');
+    res.end('Hello World  test Infra as code demo \n');
 }).listen(port);
